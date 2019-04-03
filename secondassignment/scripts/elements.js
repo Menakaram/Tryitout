ab3ce12d7a72ffb195cdc6aa3f3e69625b36e7c7
Menakaram <menuram1126@gmail.com>
2019-04-03 18:17:00 +0530

    Add files via upload

---
+++ secondassignment/scripts/elements.js
@@ -0,0 +1,13 @@
+LITTLEQAIROES-PROJECT.Elements = (function () {
+  'use strict';
+
+  function init() {
+    LITTLEQAIROES-PROJECT.Elements.Template = document.getElementById('nice-app');
+    LITTLEQAIROES-PROJECT.Elements.Loader = document.getElementById('showbox');
+    
+  }
+
+  return {
+    init: init
+  };
+}());
